ca22eaf1a422b3d3cd04990aace876635c07fe3e
Nick Adriaenssens <nick.adriaenssens@gmail.com>
2017-09-01 17:18:57 +0200

    wip

--- app/overview/overviewController.js
+++ app/overview/overviewController.js
@@ -1,15 +1,13 @@
 angular.module('myApp')
     .controller('overviewController', function($scope, $http, $routeParams) {
-       $scope.bookName = "";
-        // Simple GET request example:
+       $scope.bookName = '';
+
         $http({
             method: 'GET',
             url: 'http://localhost:8080/bookDescriptions'
         }).then(function successCallback(response) {
-            console.log("Success");
             $scope.books = response.data._embedded.bookDescriptions;
-
         }, function errorCallback(response) {
-           console.log(response);
+            console.log(response);
         });
-})
+});
